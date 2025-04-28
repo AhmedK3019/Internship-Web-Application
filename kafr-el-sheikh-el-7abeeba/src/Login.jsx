b760bf599c735c41726315becc862292119f023b
Mawlana <abdelhameedsameh44@gmail.com>
2025-04-28 13:38:14 +0300

    Updated Login

--- kafr-el-sheikh-el-7abeeba/src/Login.jsx
+++ kafr-el-sheikh-el-7abeeba/src/Login.jsx
@@ -13,44 +13,51 @@ const Login = () => {
       setMessage('Please fill in all fields.');
       return;
     }
-    setMessage(`Logged in successfully as ${role}!`);
+    setMessage(`Logged in successfully as ${role} !`);
   };
 
   return (
     <div className='page'>
-        <div className="login-container">        
-            <title/>Login
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            className="login-input"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="login-input"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <select
-            className="login-select"
-            value={role}
-            onChange={(e) => setRole(e.target.value)}
-          >
-            <option value="Company">Company</option>
-            <option value="Student">Student</option>
-            <option value="Pro Student">Pro Student</option>
-            <option value="SCAD Office Member">SCAD Office Member</option>
-            <option value="Faculty Member">Faculty Member</option>
-          </select>
-          <button type="submit">
-            Login
-          </button>
-        </form>
-        {message && <div className='message'> {message}</div>}
+      <title>Login</title>
+      <div className="login-content">        
+        <div>        
+          <form onSubmit={handleSubmit}>
+          <h2>Login to your account</h2>
+            <input
+              type="email"
+              placeholder="Email"
+              className="login-input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="login-input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <select
+              className="login-select"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <option value="Company">Company</option>
+              <option value="Student">Student</option>
+              <option value="Pro Student">Pro Student</option>
+              <option value="SCAD Office Member">SCAD Office Member</option>
+              <option value="Faculty Member">Faculty Member</option>
+            </select>
+            <button type="submit">
+              Login
+            </button>
+            {message && <div className='message'> {message}</div>}
+          </form>
+        </div>
+      </div>
+      <div className="side-panel">
+        <h2 className='side-text'>Register Your Company</h2>
+        <button>Register</button>
       </div>
     </div>
   );
