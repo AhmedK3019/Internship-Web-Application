43482d621a467dc4aa3fa7948fbed4ef2c79d3e9
Mawlana <abdelhameedsameh44@gmail.com>
2025-04-29 23:54:05 +0300

    Created Register

--- kafr-el-sheikh-el-7abeeba/src/Register.jsx
+++ kafr-el-sheikh-el-7abeeba/src/Register.jsx
@@ -1,7 +1,47 @@
+import React, { useState } from 'react';
 import './index.css';
+import Login from './Login'
+
 
 function Register(){
+  const [name, setName] = useState('');
+  const [industry, setIndustry] = useState('');
+  const [size, setSize] = useState('Small (50 employees or less)');
+  const [logo, setLogo] = useState(null);
+  const [email, setEmail] = useState('');
+  const [file, setFile] = useState(null);
 
+  function handleLogoChange(event) {
+    const file = event.target.files[0];
+    if (file) {
+      setLogo(file);
+
+      // Preview logo if you want
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    if (!name || !industry || !size || !logo || !email) {
+      setMessage('Please fill in all fields.');
+      return;
+    }
+    return(<Login/>);
+  }
+
+  return(
+    <div className='page'>
+        <form onSubmit={handleSubmit}>
+
+        </form>
+    </div>
+
+  );
 }
 
 export default Register
