0f5bd54f14da7fc6064393c072e5df565f3cc4a9
yehia4 <yehiawael2004@gmail.com>
2025-05-04 04:14:57 +0300

    hehe

---
+++ kafr-el-sheikh-el-7abeeba/src/Companies.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import GoogleImage from '../public/google.png';
+import SchneiderImage from '../public/schneider.jpg';
+import ElSewedyImage from '../public/elsewedy.png';
+
+function Companies({ onBack }) {
+  
+
+    return (
+        <div class ="page">
+            <div>
+            <h1>Suggested Companies</h1>
+                <div className="companies-page box1">
+                    <img src={GoogleImage} alt = 'Google Image' class = "GoogleImage"/>
+                    <p class = "CompanyText GoogleText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis possimus, sunt nemo nisi quae, corrupti amet impedit consequatur facere nulla asperiores laborum optio mollitia, quo ullam. Eos, fugiat sit?</p>
+                </div>
+
+                <div className="companies-page box2">
+                    <img src={SchneiderImage} alt = 'Google Image' class = "SchneiderImage"/>
+                    <p class = "CompanyText SchneiderText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis possimus, sunt nemo nisi quae, corrupti amet impedit consequatur facere nulla asperiores laborum optio mollitia, quo ullam. Eos, fugiat sit?</p>
+                </div>
+
+                <div className="companies-page box3">
+                    <img src={ElSewedyImage} alt = 'Google Image' class = "ElSewedyImage"/>
+                    <p class = "CompanyText ElSewedyText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nobis possimus, sunt nemo nisi quae, corrupti amet impedit consequatur facere nulla asperiores laborum optio mollitia, quo ullam. Eos, fugiat sit?</p>
+                </div>
+            
+                <div class = "back-button">
+                    <button onClick={onBack} className="back-button">
+                            Back to Student Dashboard
+                    </button>
+                </div>
+                
+            
+            </div>
+             
+            
+        </div>
+        
+    );
+}
+
+export default Companies;
--- kafr-el-sheikh-el-7abeeba/src/Student.jsx
+++ kafr-el-sheikh-el-7abeeba/src/Student.jsx
@@ -1,46 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
+import ViewSuggestedCompanies from './ViewSuggestedCompanies';
 import './index.css';
 
-function Student({ email }) {
+function Student({ email, onViewCompanies }) {
   const username = email.split('@')[0];
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const suggestions = [
-    { name: "Valeo", industry: "Automotive", note: "Recommended by 5 interns" },
-    { name: "Microsoft", industry: "Tech", note: "Great for software development" },
-    { name: "Orange", industry: "Telecom", note: "Popular among students" },
-    { name: "IBM", industry: "IT Consulting", note: "Known for training programs" }
-  ];
-
-  function toggleDropdown() {
-    setShowDropdown(!showDropdown);
-  }
 
   return (
     <div className="page">
-      <header>
-        <h1>Welcome {username}</h1>
-      </header>
+      <div className="student-container">
+        <header className="student-header">
+          <h1>Welcome {username}</h1>
+        </header>
 
-      <div className="content">
-        <button className="suggestion-button" onClick={toggleDropdown}>
-          {showDropdown ? 'Hide Suggested Companies' : 'View Suggested Companies'}
-        </button>
-
-        {showDropdown && (
-          <ul className="dropdown">
-            {suggestions.map((company, index) => (
-              <li key={index} className="dropdown-item">
-                <strong>{company.name}</strong> - {company.industry}
-                <br />
-                <small>{company.note}</small>
-              </li>
-            ))}
-          </ul>
-        )}
+        <div className="companies-wrapper">
+          <ViewSuggestedCompanies onViewCompanies={onViewCompanies} />
+        </div>
       </div>
     </div>
   );
 }
 
-export default Student;
+export default Student; 
