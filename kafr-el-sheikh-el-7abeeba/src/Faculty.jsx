8f938e8902a26224833e4dbef1f0e4a37b9f6ec4
Ahmed1Elsayed <ahmed.m.elsayed20@gmail.com>
2025-05-06 14:52:26 +0300

    adding faculty member menu

--- kafr-el-sheikh-el-7abeeba/src/Faculty.jsx
+++ kafr-el-sheikh-el-7abeeba/src/Faculty.jsx
@@ -1,57 +1,67 @@
 import React, { useState } from 'react';
-import ViewInternshipReports from "./ViewInternshipReports";
 import InternshipReports from "./InternshipReports";
-import ViewEvaluationReports from "./ViewEvaluationReports";
 import EvaluationReports from "./EvaluationReports";
+import Statistics from "./Statistics";
 import './index.css';
 
 function Faculty({email}){
-    const username = email.split("@")[0];
-    const [showInternshipReports, setShowInternshipReports] = useState(false);
-    const [showEvaluationReports, setShowEvaluationReports] = useState(false);
-
-
-  const handleViewInternshipReports = () => {
-    setShowInternshipReports(true);
-  };
-  
-  const handleBack1 = () => {
-    setShowInternshipReports(false);
-  };
-  
-  if (showInternshipReports) {
-    return <InternshipReports onBack={handleBack1} />;
-  }
-
-
-
-  const handleViewEvaluationReports = () => {
-    setShowEvaluationReports(true);
-  };
-  
-  const handleBack2 = () => {
-    setShowEvaluationReports(false);
-  };
-
-  if (showEvaluationReports) {
-    return <EvaluationReports onBack={handleBack2} />;
-  }
+     const username = email.split("@")[0];
+      const [currentView, setCurrentView] = useState("");
+     
+    
+      const handleBackToDashboard = () => {
+        setCurrentView("");
+      };
+    
+    
 
   
-    return (
-        <div className="page">
+  return (
+    <div className="page">
+      <div className="sidebar">
+        <div className="sidebar-header">
+          <div className="menu-wrapper">
+            <span className="menu-icon">☰</span>
+            <span className="menu-label">Faculty Member</span>
+          </div>
+        </div>
+        <div className="sidebar-buttons">
+          <button onClick={() => setCurrentView("")}>
+            Dashboard
+          </button>
+          <button onClick={() => setCurrentView("internshipreports")}>
+            View Internship Reports
+          </button>
+          <button onClick={() => setCurrentView("evaluationreports")}>
+            View Evaluation reports
+          </button>
+          <button onClick={() => setCurrentView("statistics")}>
+            Statistics
+          </button>
+         
+        </div>
+      </div>
+
+      <div className="main-content">
+        {currentView === "" && (
           <div className="faculty-container">
             <header className="faculty-header">
               <h1>Welcome {username}</h1>
+              <hr />
+              
             </header>
-            
-            <div>
-            <ViewInternshipReports onViewInternshipReports={handleViewInternshipReports} />
-            <ViewEvaluationReports onViewEvaluationReports={handleViewEvaluationReports} />
-            </div>
           </div>
-        </div>
-      );
+        )}
+        
+        {currentView === "internshipreports" && <InternshipReports onBackInternshipreports={handleBackToDashboard} />}
+        {currentView === "evaluationreports" && <EvaluationReports onBackEvaluationreports={handleBackToDashboard} />}
+        {currentView === "statistics" && <Statistics onBackStatistics={handleBackToDashboard} />}
+       
+        
+        
+      </div>
+    </div>
+  );
     
 }
 
---
+++ kafr-el-sheikh-el-7abeeba/src/Statistics.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Statistics({ onBack }) {
+  
+
+    return (
+        <div class ="page">
+            <div>
+            <h1>Statistics</h1>
+            </div>
+             
+            
+             </div>
+             
+         );
+     }
+     
+     export default Statistics;
