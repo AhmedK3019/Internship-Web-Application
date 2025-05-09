ba8c28dcae1bbddae802aa207b1e1eb873915383
Mawlana <abdelhameedsameh44@gmail.com>
2025-05-09 18:23:18 +0300

    Made users shared

--- kafr-el-sheikh-el-7abeeba/src/Login.jsx
+++ kafr-el-sheikh-el-7abeeba/src/Login.jsx
@@ -7,6 +7,7 @@ import Student from "./Student";
 import ProStudent from "./ProStudent";
 import Faculty from "./Faculty";
 import companiesRequests from "./companiesRequests";
+import users from "./users";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -81,55 +82,6 @@ function Login() {
     localStorage.removeItem("user");
     localStorage.removeItem("view");
   }
-
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      name: "John Doe",
-      email: "JohnDoe@gmail.com",
-      password: "johndoe123",
-      role: "Student",
-    },
-    {
-      id: 2,
-      name: "Jane Smith",
-      email: "JaneSmith@gmail.com",
-      password: "janesmith123",
-      role: "Student",
-    },
-    {
-      id: 3,
-      name: "Alice Johnson",
-      email: "AliceJohnson@gmail.com",
-      password: "alicejohnson123",
-      role: "Pro Student",
-    },
-    {
-      id: 4,
-      name: "Bob Brown",
-      email: "BobBrown@gmail.com",
-      password: "bobbrown123",
-      role: "Company",
-      company: "Sumerge",
-      industry: "IT",
-      size: "Medium",
-      logo: "sumerge.jpeg",
-    },
-    {
-      id: 5,
-      name: "Charlie Davis",
-      email: "CharlieDavis@gmail.com",
-      password: "charliedavis123",
-      role: "SCAD Office Member",
-    },
-    {
-      id: 6,
-      name: "David Wilson",
-      email: "DavidWilson@gmail.com",
-      password: "davidwilson123",
-      role: "Faculty Member",
-    },
-  ]);
 
   async function onRegister(companyRequest) {
     try {
---
+++ kafr-el-sheikh-el-7abeeba/src/users.js
@@ -0,0 +1,50 @@
+const users = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "JohnDoe@gmail.com",
+      password: "johndoe123",
+      role: "Student",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      email: "JaneSmith@gmail.com",
+      password: "janesmith123",
+      role: "Student",
+    },
+    {
+      id: 3,
+      name: "Alice Johnson",
+      email: "AliceJohnson@gmail.com",
+      password: "alicejohnson123",
+      role: "Pro Student",
+    },
+    {
+      id: 4,
+      name: "Bob Brown",
+      email: "BobBrown@gmail.com",
+      password: "bobbrown123",
+      role: "Company",
+      company: "Sumerge",
+      industry: "IT",
+      size: "Medium",
+      logo: "sumerge.jpeg",
+    },
+    {
+      id: 5,
+      name: "Charlie Davis",
+      email: "CharlieDavis@gmail.com",
+      password: "charliedavis123",
+      role: "SCAD Office Member",
+    },
+    {
+      id: 6,
+      name: "David Wilson",
+      email: "DavidWilson@gmail.com",
+      password: "davidwilson123",
+      role: "Faculty Member",
+    },
+];
+
+export default users;
